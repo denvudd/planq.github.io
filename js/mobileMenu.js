0a8047fb03139b4337f3c8a73f33c21b2aa74014
denvudd <68691654+denvudd@users.noreply.github.com>
2021-10-02 15:26:08 +0300

    Созданы 3 блока с адаптивом, мобильное меню

---
+++ js/mobileMenu.js
@@ -0,0 +1,24 @@
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu__button');
+  let links = menu.find('.burger__link');
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on('click', () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass('burger-menu__active');
+
+    if (menu.hasClass('burger-menu__active')) {
+      $('body').css('overflow-y', 'hidden');
+    } else {
+      $('body').css('overflow-y', 'visible');
+    }
+  }
+}
+
+burgerMenu('.burger-menu');
